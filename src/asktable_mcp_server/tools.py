a0d0237f3fbd7035acb70e528886a9e708c8adb9
lele <lele@datamini.ai>
2025-07-07 09:03:26 +0800

    remove asktable helper

--- src/asktable_mcp_server/tools.py
+++ src/asktable_mcp_server/tools.py
@@ -1,10 +1,9 @@
 from asktable import Asktable
-
-from .utils import AskTableHelper
+import json
 
 
 async def get_asktable_data(
-    api_key, datasource_id, question, base_url=None, role_id=None
+    api_key, datasource_id, question, base_url=None, role_id: str=None, role_variables: dict = None
 ):
     """
     获取asktable数据
@@ -13,12 +12,16 @@ async def get_asktable_data(
     :param question:
     :param base_url:
     :param role_id:
+    :param role_variables:
     :return:
     """
 
     asktable_client = Asktable(api_key=api_key, base_url=base_url)
     answer_response = asktable_client.answers.create(
-        datasource_id=datasource_id, question=question, role_id=role_id
+        datasource_id=datasource_id, 
+        question=question, 
+        role_id=role_id,
+        role_variables=role_variables
     )
     if answer_response.answer is None:
         return "没有查询到相关信息"
@@ -26,12 +29,15 @@ async def get_asktable_data(
 
 
 async def get_asktable_sql(
-    api_key, datasource_id, question, base_url=None, role_id=None
+    api_key, datasource_id, question, base_url=None, role_id: str=None, role_variables: dict = None
 ):
 
     asktable_client = Asktable(api_key=api_key, base_url=base_url)
     query_response = asktable_client.sqls.create(
-        datasource_id=datasource_id, question=question, role_id=role_id
+        datasource_id=datasource_id, 
+        question=question, 
+        role_id=role_id,
+        role_variables=role_variables
     )
     if query_response.query is None:
         return "没有查询到相关信息"
@@ -46,13 +52,31 @@ async def get_datasources_info(api_key, base_url=None):
     args:
         api_key: str
         base_url: str
-        role_id:str
     return:
         {
             "status": "success" or "failure",
             "data": json.dumps(result, ensure_ascii=False, indent=2)
         }
     """
-    helper = AskTableHelper(api_key=api_key, base_url=base_url)
-    return helper.get_datasources_info()
+    asktable_client = Asktable(api_key=api_key, base_url=base_url)
+
+    meta_data_list = asktable_client.datasources.list()
+    if not meta_data_list.items:
+        return {
+            "status": "failure",
+            "data": "该用户还没有创建任何数据库"
+        }
+    result = [
+        {
+            "datasource_id": ds.id,
+            "数据库名称": ds.name,
+            "数据库引擎": ds.engine,
+            "数据库描述": ds.desc,
+        }
+        for ds in meta_data_list.items
+    ]
+    return {
+        "status": "success",
+        "data": json.dumps(result, ensure_ascii=False, indent=2)
+    }
 
